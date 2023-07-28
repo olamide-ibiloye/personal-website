16bc1089556650cf2062189c25d6958e8f7ea033
olamide-ibiloye <ibiloyeolamide@gmail.com>
2023-07-28 17:44:15 +0100

    eslint and prettier set up

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,11 +3,11 @@ import type { HeadFC, PageProps } from "gatsby";
 import { Box, Typography } from "@mui/material";
 
 const HomePage: React.FC<PageProps> = () => {
-    return (
-        <Box>
-            <Typography variant="h1">HomePage with Olamide</Typography>
-        </Box>
-    );
+  return (
+    <Box>
+      <Typography variant="h1">HomePage with Olamide</Typography>
+    </Box>
+  );
 };
 
 export default HomePage;
